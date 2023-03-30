0cdea435dd997b081a50c648098d94a8ed11a9e2
Luiz Henrique Rodrigues Mendes <lh.m21112000@gmail.com>
2023-03-30 14:07:41 -0300

    atualização v2

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 //-----------------------CONEXAO COM O BANCO DE DADOS MONGODB----------------------------------------
 //entregar uma porta para o servidor e conectar ao banco de dados
-const DB_User = process.env.USER.DB //usuario do banco de dados
-const DB_Pass = process.env.PASSWORD_DB //senha do usuario do banco de dados
-mongoose.connect(`mongodb+srv://${DB_User}:${DB_Pass}@apicluster.pbksx7x.mongodb.net/?retryWrites=true&w=majority`)
+const PRE_DB = process.env.PREFIXO_DB
+const URL_DB = process.env.URL_DB
+const DB_User = process.env.USER.DB 
+const DB_Pass = process.env.PASSWORD_DB 
+mongoose.connect(`${PRE_DB}//${DB_User}:${DB_Pass}@${URL_DB}`)
     .then(() => {
         console.log('Conectado!')
     })
