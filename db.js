b1aa0ebd8505ae439d2d28cbd619a848ac7f228a
Luiz Henrique Rodrigues Mendes <lh.m21112000@gmail.com>
2023-04-04 10:02:41 -0300

    atualizacao

--- db.js
+++ db.js
@@ -2,11 +2,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 //-----------------------CONEXAO COM O BANCO DE DADOS MONGODB----------------------------------------
 //entregar uma porta para o servidor e conectar ao banco de dados
-//const DB_User = process.env.USER_DB //usuario do banco de dados
-//const DB_Pass = process.env.PASSWORD_DB //senha do usuario do banco de dados
-const DB_LOCAL = process.env.DB_LOCAL //senha do usuario do banco de dados
+const DB_User = process.env.DB_USER //usuario do banco de dados
+const DB_Pass = process.env.DB_PASS//senha do usuario do banco de dados
+const DB_Name = process.env.DB_NAME
+const DB_Host = process.env.DB_HOST
+const DB_Port = process.env.DB_PORT
+const url = `mongodb://${DB_User}:${DB_Pass}@${DB_Host}:${DB_Port}/${DB_Name}`;
 mongoose
-  .connect(DB_LOCAL)
+
+mongoose
+  .connect(url)
   .then(() => {
     console.log("Conectado!");
   })
