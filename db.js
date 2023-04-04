818978820437a05dd1712c6bc0efb29185558858
Luiz Henrique Rodrigues Mendes <lh.m21112000@gmail.com>
2023-04-04 10:16:19 -0300

    asdkasdk

--- db.js
+++ db.js
@@ -2,16 +2,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 //-----------------------CONEXAO COM O BANCO DE DADOS MONGODB----------------------------------------
 //entregar uma porta para o servidor e conectar ao banco de dados
-const DB_User = process.env.DB_USER //usuario do banco de dados
-const DB_Pass = process.env.DB_PASS//senha do usuario do banco de dados
-const DB_Name = process.env.DB_NAME
-const DB_Host = process.env.DB_HOST
-const DB_Port = process.env.DB_PORT
-const url = `mongodb://${DB_User}:${DB_Pass}@${DB_Host}:${DB_Port}/${DB_Name}`;
+//const DB_User = process.env.USER_DB //usuario do banco de dados
+//const DB_Pass = process.env.PASSWORD_DB //senha do usuario do banco de dados
+const DB = process.env.DB_LOCAL //senha do usuario do banco de dados
 mongoose
-
-mongoose
-  .connect(url)
+  .connect(DB)
   .then(() => {
     console.log("Conectado!");
   })
